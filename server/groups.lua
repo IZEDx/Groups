fe49c3afbe8bcbcecaa99b9ceee6c07e4e9feb94
IZED <admin@i-zed.net>
2013-12-22 17:31:31 +0100

    First Release

---
+++ server/groups.lua
@@ -0,0 +1,457 @@
+
+function SendChatMessage(ply, ...)
+	args = {...}
+	if type(args[1]) == "table" then
+		Events:Fire("ZEDSendChatMessage", {player=ply, message=args[1]})
+	else
+		Events:Fire("ZEDSendChatMessage", {player=ply, message={...}})
+	end
+end
+
+Groups = {}
+CreateGroup = function(name, permission, inherits, color)
+	grp = {}
+	grp.name = name
+	grp.permission = permission
+	grp.inherits = inherits
+	grp.color = color
+	table.insert(Groups, grp)
+end
+ParseColor = function(t)
+	if(t)then
+		return Color(t[1],t[2],t[3])
+	end
+end
+GetGroup = function(g)
+	for _,grp in pairs(Groups) do
+		if(grp.name == g)then
+			return grp
+		end
+	end
+end
+GroupHasPermission = function(grp, str)
+	if(GetGroup(grp))then
+		if GetGroup(grp).inherits then
+			if(GroupHasPermission(GetGroup(grp).inherits, str))then 
+				return true 
+			end
+		end
+		for k,v in pairs(GetGroup(grp).permission) do
+			if(v == "*")then return true end
+			if(string.lower(str) == string.lower(v))then
+				return true
+			end
+		end
+		return false
+	else
+		return false
+	end
+end
+GetPlayerGroup = function(ply)
+		local pdata = PData:Get(ply)
+		if(pdata)then
+				for _,grp in pairs(Groups) do
+						if(strEquals(grp.name, pdata.group))then
+								return grp
+						end
+				end
+		end
+end
+GroupExists = function(str)
+		for _,grp in pairs(Groups) do
+				if(strEquals(grp.name, str))then
+						return true
+				end
+		end
+		return false
+end
+FindGroup = function(str)
+		for _,grp in pairs(Groups) do
+				if(strFind(grp.name, str))then
+						return grp
+				end
+		end
+		return false
+end
+GetPlayer = function(str)
+	for player in Server:GetPlayers() do
+		if(string.find(string.lower(player:GetName()), string.lower(str)))then
+			return player
+		end
+	end
+end
+strFind = function(v1, v2)
+	if(string.find(string.lower(tostring(v1)), string.lower(tostring(v2))))then
+		return true
+	else
+		return false
+	end
+end
+strEquals = function(v1, v2)
+	if(string.lower(tostring(v1)) == string.lower(tostring(v2)))then
+		return true
+	else
+		return false
+	end
+end
+
+
+Events:Subscribe("ZEDExecuteCommand", function(a)
+	local ply = a.player
+	local args = a.cmd
+	if(strEquals("setgroup", args[1]))then
+		if(args[2] and args[3])then
+			if GetPlayer(args[2]) then
+				target = GetPlayer(args[2])
+				if(FindGroup(args[3]))then
+					PData:Set(target, {group = FindGroup(args[3]).name})
+					SendChatMessage(ply, Color(0,200,0,255),"Set group from " .. target:GetName() .. " to " .. FindGroup(args[3]).name, Color(0,200,0,255))
+					SendChatMessage(target, Color(0,200,0,255),"Your group has been set to " .. FindGroup(args[3]).name)
+				else
+					SendChatMessage(ply, Color(200,0,0,255),"Can't find group " .. args[3])
+				end
+			else
+				SendChatMessage(ply, Color(200,0,0,255),"Can't find " .. args[2], Color(200,0,0,255))
+			end
+		else
+			SendChatMessage(ply, Color(200,0,0,255),"Syntax: /setgroup <player> <group>", Color(200,0,0,255))
+		end
+	end
+	if(strEquals("group", args[1]))then
+		if(args[2] and args[3])then
+			if(strFind(args[2], "create"))then
+				if(GroupExists(args[3]))then
+					SendChatMessage(ply, Color(200,0,0,255),"Group already exists: " .. args[3])
+				else
+					if(FindGroup(args[4]))then
+						CreateGroup(args[3], {}, FindGroup(args[4]).name)
+					else
+						CreateGroup(args[3], {})
+					end
+					local str = json():encode(FindGroup(args[3]))
+					local file = io.open("./data/groups/"..args[3]..".txt", "w")
+					file:write(str)
+					file:close()
+					SendChatMessage(ply, Color(0,200,0,255),"Group created: " .. args[3], Color(0,200,0,255))
+				end
+			elseif(strFind(args[2], "delete"))then
+				if(FindGroup(args[3]))then
+					local name = FindGroup(args[3]).name
+					for k,v in pairs(Groups) do
+						if(strEquals(v.name, name))then
+							local file = io.open("./data/groups/"..name..".txt", "w")
+							file:write("Deleted by " .. ply:GetName())
+							file:close()
+							Groups[k] = nil
+							SendChatMessage(ply, Color(0,200,0,255),"Group deleted: " .. name)
+							break
+						end
+					end
+				else
+					SendChatMessage(ply, Color(200,0,0,255),"Can't find " .. args[3], Color(200,0,0,255))
+				end
+			elseif(strFind(args[2], "addperm"))then
+				if(FindGroup(args[3]))then
+					if(not args[4])then
+						SendChatMessage(ply, Color(200,0,0,255),"Syntax: /group addperm <group> <permission>", Color(200,0,0,255))
+						return
+					end
+					for k,v in pairs(Groups) do
+						if(v.name == FindGroup(args[3]).name)then
+							for i,j in pairs(Groups[k].permission) do
+								if(strEquals(j, args[4]))then
+									SendChatMessage(ply, Color(200,0,0,255),"This group has already this permission: " .. args[4])
+									return
+								end
+							end
+							table.insert(Groups[k].permission, args[4])
+						end
+					end
+					local str = json():encode(FindGroup(args[3]))
+					local file = io.open("./data/groups/"..FindGroup(args[3]).name..".txt", "w")
+					file:write(str)
+					file:close()
+					SendChatMessage(ply, Color(0,200,0,255),"Permission added: " .. args[4])
+				else
+					SendChatMessage(ply, Color(200,0,0,255),"Can't find " .. args[3])
+				end
+			elseif(strFind(args[2], "delperm"))then
+				if(FindGroup(args[3]))then
+					if(not args[4])then
+						SendChatMessage(ply, Color(200,0,0,255), "Syntax: /group delperm <group> <permission>")
+						return
+					end
+					local found = -1
+					local group = -1
+					local perm = ""
+					for k,v in pairs(Groups) do
+						if(v.name == FindGroup(args[3]).name)then
+							group = k
+							for i,j in pairs(Groups[k].permission) do
+								if(strFind(j, args[4]))then
+									found = i
+									perm = j
+								end
+							end
+							break
+						end
+					end
+					if(found > -1)then
+						Groups[group].permission[found]=nil
+						local str = json():encode(FindGroup(args[3]))
+						local file = io.open("./data/groups/"..FindGroup(args[3]).name..".txt", "w")
+						file:write(str)
+						file:close()
+						SendChatMessage(ply, Color(0,200,0,255),"Permission removed: " .. perm)
+					else
+						SendChatMessage(ply, Color(200,0,0,255),"Permission not found: " .. args[4])
+					end
+				else
+					SendChatMessage(ply, Color(200,0,0,255),"Can't find " .. args[3])
+				end
+			elseif(strFind(args[2], "setcolor"))then
+				if(FindGroup(args[3]))then
+					if (not args[4]) or (not args[5]) or (not args[6]) then
+						SendChatMessage(ply, Color(200,0,0,255),"Syntax: group setcolor <group> <r> <g> <b>")
+						return
+					end
+					for k,v in pairs(Groups) do
+						if(v.name == FindGroup(args[3]).name)then
+							Groups[k].color = {tonumber(args[4]), tonumber(args[5]), tonumber(args[6])}
+						end
+					end
+					local str = json():encode(FindGroup(args[3]))
+					local file = io.open("./data/groups/"..FindGroup(args[3]).name..".txt", "w")
+					file:write(str)
+					file:close()
+					SendChatMessage(ply, Color(tonumber(args[4]), tonumber(args[5]), tonumber(args[6])),"Color set.")
+				else
+					SendChatMessage(ply, Color(200,0,0,255),"Can't find " .. args[3])
+				end
+			end
+		else
+			SendChatMessage(ply, Color(200,0,0,255),"Syntax: /group create <name> <inherits>")
+			SendChatMessage(ply, Color(200,0,0,255),"Syntax: /group delete <name>")
+			SendChatMessage(ply, Color(200,0,0,255),"Syntax: /group addperm <group> <permission>")
+			SendChatMessage(ply, Color(200,0,0,255),"Syntax: /group delperm <group> <permission>")
+			SendChatMessage(ply, Color(200,0,0,255),"Syntax: /group setcolor <group> <r> <g> <b>")
+			if(#Groups > 0)then
+				SendChatMessage(ply, Color(0,180,130),"Available Groups:")
+				local c = 0
+				local str = ""
+				for k,v in pairs(Groups) do
+					c = c + 1
+					str = str .. ", " .. v.name
+					if (c == 5)then
+						c = 0
+						SendChatMessage(ply, Color(0,200,150),string.sub(str, 3))
+						str = ""
+					end
+				end
+				if(c > 0)then
+					SendChatMessage(ply, Color(0,200,150),string.sub(str, 3))
+				end
+			end
+		end
+	end
+end)
+
+Console:Subscribe("setgroup", function(args)
+	if(args[1] and args[2])then
+		if GetPlayer(args[1]) then
+			target = GetPlayer(args[1])
+			if(FindGroup(args[2]))then
+				PData:Set(target, {group = FindGroup(args[2]).name})
+				print("Set group from " .. target:GetName() .. " to " .. FindGroup(args[2]).name)
+				SendChatMessage(target, Color(0,200,0), "Your group has been set to " .. FindGroup(args[2]).name)
+			else
+				print("Can't find group " .. args[2])
+			end
+		else
+			print("Can't find " .. args[1])
+		end
+	else
+		print("Syntax: /setgroup <player> <group>")
+	end
+end)
+
+Console:Subscribe("group", function(args)
+	if(args[1] and args[2])then
+		if(strFind(args[1], "create"))then
+			if(GroupExists(args[2]))then
+				print("Group already exists: " .. args[2])
+			else
+				if(FindGroup(args[3]))then
+					CreateGroup(args[2], {}, FindGroup(args[3]).name)
+				else
+					CreateGroup(args[2], {})
+				end
+				local str = json():encode(FindGroup(args[2]))
+				local file = io.open("./data/groups/"..args[2]..".txt", "w")
+				file:write(str)
+				file:close()
+				print("Group created: " .. args[2])
+			end
+		elseif(strFind(args[1], "delete"))then
+			if(FindGroup(args[2]))then
+				local name = FindGroup(args[2]).name
+				for k,v in pairs(Groups) do
+					if(strEquals(v.name, name))then
+						local file = io.open("./data/groups/"..name..".txt", "w")
+						file:write("Deleted by " .. ply:GetName())
+						file:close()
+						Groups[k] = nil
+						print("Group deleted: " .. name)
+						break
+					end
+				end
+			else
+				print("Can't find " .. args[2])
+			end
+		elseif(strFind(args[1], "addperm"))then
+			if(FindGroup(args[2]))then
+				if(not args[3])then
+					print("Syntax: group addperm <group> <permission>")
+					return
+				end
+				for k,v in pairs(Groups) do
+					if(v.name == FindGroup(args[2]).name)then
+						for i,j in pairs(Groups[k].permission) do
+							if(strEquals(j, args[3]))then
+								print("This group has already this permission: " .. args[3])
+								return
+							end
+						end
+						table.insert(Groups[k].permission, args[3])
+					end
+				end
+				local str = json():encode(FindGroup(args[2]))
+				local file = io.open("./data/groups/"..FindGroup(args[2]).name..".txt", "w")
+				file:write(str)
+				file:close()
+				print("Permission added: " .. args[3])
+			else
+				print("Can't find " .. args[2])
+			end
+		elseif(strFind(args[1], "delperm"))then
+			if(FindGroup(args[2]))then
+				if(not args[3])then
+					print("Syntax: group delperm <group> <permission>")
+					return
+				end
+				local found = -1
+				local group = -1
+				local perm = ""
+				for k,v in pairs(Groups) do
+					if(v.name == FindGroup(args[2]).name)then
+						group = k
+						for i,j in pairs(Groups[k].permission) do
+							if(strFind(j, args[3]))then
+								found = i
+								perm = j
+							end
+						end
+						break
+					end
+				end
+				if(found > -1)then
+					Groups[group].permission[found]=nil
+					local str = json():encode(FindGroup(args[2]))
+					local file = io.open("./data/groups/"..FindGroup(args[2]).name..".txt", "w")
+					file:write(str)
+					file:close()
+					print("Permission removed: " .. perm)
+				else
+					print("Permission not found: " .. args[3])
+				end
+			else
+				print("Can't find " .. args[2])
+			end
+		elseif(strFind(args[1], "setcolor"))then
+			if(FindGroup(args[2]))then
+				if (not args[3]) or (not args[4]) or (not args[5]) then
+					print("Syntax: group setcolor <group> <r> <g> <b>")
+					return
+				end
+				for k,v in pairs(Groups) do
+					if(v.name == FindGroup(args[2]).name)then
+						Groups[k].color = {args[3], args[4], args[5]}
+					end
+				end
+				local str = json():encode(FindGroup(args[2]))
+				local file = io.open("./data/groups/"..FindGroup(args[2]).name..".txt", "w")
+				file:write(str)
+				file:close()
+				print("Color set.")
+			else
+				print("Can't find " .. args[2])
+			end
+		end
+	else
+		print("Syntax: group create <name> <inherits>")
+		print("Syntax: group delete <name>")
+		print("Syntax: group addperm <group> <permission>")
+		print("Syntax: group delperm <group> <permission>")
+		print("Syntax: group setcolor <group> <r> <g> <b>")
+		if(#Groups > 0)then
+			print("Available Groups:")
+			local c = 0
+			local str = ""
+			for k,v in pairs(Groups) do
+				c = c + 1
+				str = str .. ", " .. v.name
+				if (c == 5)then
+					c = 0
+					print(string.sub(str, 3))
+					str = ""
+				end
+			end
+			if(c > 0)then
+				print(string.sub(str, 3))
+			end
+		end
+	end
+end)
+
+Events:Subscribe("ZEDScoreboardUpdate", function()
+	local tbl = {}
+	for player in Server:GetPlayers() do
+		if(GetPlayerGroup(player))then
+			tbl[player:GetId()] = GetPlayerGroup(player).name
+		end
+	end
+	Events:Fire("ZEDUpdateScoreboard", {Group=tbl})
+end)
+	
+Events:Subscribe("ZEDPlayerInit", function(args)
+	PData:Load(args.player, {group="User"})
+	if(not PData:Get(args.player).group)then
+		PData:Set(args.player, {group = "User"})
+	end
+end)
+
+Events:Subscribe("PlayerQuit", function(args)
+	PData:Save(args.player)
+end)
+
+Events:Subscribe("ZEDPlayerHasPermission", function(args)
+	if(GroupHasPermission(GetPlayerGroup(args.player).name, args.permission))then
+		return false
+	end
+end)
+
+Events:Subscribe("ModulesLoad", function()
+	for k,v in pairs(io.files("./data/groups")) do
+		local file = io.open("./data/groups/" .. v, "r")
+		local ret = json():decode(file:read("*all"))
+		if(ret)then
+			CreateGroup(string.sub(v, 1, -5), ret.permission, ret.inherits, ret.color)
+		end
+	end
+end)
+
+Events:Subscribe("ModuleUnload", function()
+	for player in Server:GetPlayers() do
+		PData:Save(player)
+	end
+end)
---
+++ server/pdata.lua
@@ -0,0 +1,88 @@
+PData = {}
+PData.Players = {} 
+PData.Test = "FUCK THIS"
+
+file_exists =  function (t, name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end
+PData.Get = function(t,ply)
+	if(PData.Players[ply:GetId()])then
+		return PData.Players[ply:GetId()]
+	else
+		return {}
+	end
+end
+PData.Set = function(t,ply, tbl)
+	for k,v in pairs(tbl) do
+		PData.Players[ply:GetId()][k] = v
+	end
+end
+
+PData.Exists = function( t,ply)
+	if(file_exists("./data/player/" .. string.gsub(tostring(ply:GetSteamId()), ":", "-") .. ".txt"))then
+		return true
+	else
+		return false
+	end
+end
+
+PData.Save = function(t,ply, tbl)
+	if(tbl)then
+		local str = json():encode(tbl)
+		if(PData:Get(ply))then
+				local p = PData:Get(ply)
+				for k,v in pairs(tbl) do
+						p[k] = v
+				end
+				str = json():encode(p)
+		end
+		local file = io.open("./data/player/" .. string.gsub(tostring(ply:GetSteamId()), ":", "-") .. ".txt", "w")
+		file:write(str)
+		file:close()
+	else
+		local str = json():encode({})
+		if(PData:Get(ply))then
+			str = json():encode(PData:Get(ply))
+		end
+		local file = io.open("./data/player/" .. string.gsub(tostring(ply:GetSteamId()), ":", "-") .. ".txt", "w")
+		file:write(str)
+		file:close()
+	end
+end
+
+PData.Load = function(t,ply, default)
+	local file = io.open("./data/player/" .. string.gsub(tostring(ply:GetSteamId()), ":", "-") .. ".txt", "r")
+	if(file)then
+		local json = json()
+		local ret = json:decode(file:read("*all"))
+		if(ret)then
+			if(default)then
+				for k,v in pairs(default) do
+					local found = false
+					for i,j in pairs(ret) do
+						if i == k then
+							found = true
+						end
+					end
+					if not found then
+						ret[k] = v
+					end
+				end
+			end
+			PData.Players[ply:GetId()] = ret
+		else
+			if(default)then
+				PData.Players[ply:GetId()] = default
+			else
+				PData.Players[ply:GetId()] = {}
+			end
+		end
+	else
+		if(default)then
+			PData.Players[ply:GetId()] = default
+		else
+			PData.Players[ply:GetId()] = {}
+		end
+	end
+end
